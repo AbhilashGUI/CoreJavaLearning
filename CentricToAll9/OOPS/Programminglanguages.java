75110dc4fbcfc979a7187534d55adc8996cde5e7
AbhilashGUI <vemulaabhilash8433@gmail.com>
2024-01-11 22:59:31 +0530

    Practiced and pushed the code

---
+++ CentricToAll9/OOPS/Programminglanguages.java
@@ -0,0 +1,49 @@
+package CentricToAll9.OOPS;
+
+public class Programminglanguages
+{
+    //Attributes
+
+    String language;
+
+    int Foundedyear;
+
+    //Note: Bydefault, Default constructor will be executed and shown as output
+
+    /*Programminglanguages()
+    {
+        System.out.println("Default constructor");
+    }*/
+
+    Programminglanguages(String lang,int FOY)
+    {
+        this.language=lang;
+        this.Foundedyear=FOY;
+    }
+
+
+
+
+
+
+
+    void printinfo()
+    {
+        //System.out.println(language);
+        //System.out.println(Foundedyear);
+        System.out.println(" Programming info : " + "Language-->" + this.language + " Founded in the year---> " + this.Foundedyear);
+    }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
