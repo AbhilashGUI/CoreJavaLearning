a60cc304f899f2fc07805500993fe43ace3ba2c3
AbhilashGUI <vemulaabhilash8433@gmail.com>
2024-01-13 23:03:33 +0530

    Practiced and pushed the code

---
+++ AssignmentPrograms/Program34.java
@@ -0,0 +1,28 @@
+package AssignmentPrograms;
+
+public class Program34
+{
+    public static void main(String[] args)
+    {
+
+        //Count the length of string
+
+        String name="Abhilash";
+        System.out.println(name.length());
+
+
+       //Count the length of an integer: Convert it to string and then retrieve using a length method.
+
+        int a=4554;
+        String s=Integer.toString(4554);
+        int length=s.length();
+        System.out.println(length);
+
+
+
+
+
+    }
+}
+
+
