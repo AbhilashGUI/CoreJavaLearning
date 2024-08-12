f9e9948719fd82aec15878a65ddbb32e222f7559
AbhilashGUI <vemulaabhilash8433@gmail.com>
2024-08-12 10:48:02 +0530

    Reupdated code

--- CentricToAll11/CustomizedExceptions/Lab185.java
+++ CentricToAll11/CustomizedExceptions/Lab185.java
@@ -1,24 +1,32 @@
 package CentricToAll11.CustomizedExceptions;
 
-public class Lab185
-{
-    public static void main(String[] args) throws ArithmeticException {
+public class Lab185 {
+    public static void main(String[] args) throws Exception {
 
-        //extracted(10);       //Extracting the method by generating using shortcuts. Passing a param/argument.
-        extracted(0);
-        throw new ArithmeticException("Handling the exception at the method level");   //Adding a throw keyword, which is vulnerable to this code.
+        //int a=10/0;
+        //System.out.println(a);   // In this case, JVM will throw an exception
+
+
+        //Note: Throw keyword is used at the object level and throws keyword is used at the method level
+        //Note:We are using  throw and throws keyword, in-order to create a customized exception
+
+        //Here we are extracting method using private keyword
+
+        extracted(0);   //Passing parameter
+        throw new Exception("Combination of throw and throws keyword");
+        //Adding a throw keyword, which is vulnerable to this code.
+
+
     }
 
-    private static void extracted(int b) {
-        int a;
-        if(b==0){
-            throw new ArithmeticException("B can't be executed");
-        }
-        else{
-            a =10/b;
-        }
+    private static void extracted(int c) {
+        int b = 0;
+        if (c == 0) {
+            throw new ArithmeticException("Intentionally failing the case");
+        } else {
+            b = 10 / c;
+            System.out.println(b);
 
-        System.out.println(a);                      //It throws a default Arithmetic exception by JVM
+        }
     }
 }
-//Note:We are using the throw and throws keyword, in-order to create a customized exception
