9f0875fc335eab90bcbe786c14c5cc9199d5bdd2
AbhilashGUI <vemulaabhilash8433@gmail.com>
2024-08-13 10:10:24 +0530

    Reupdated code

--- CentricToAll11/CustomizedExceptions/Lab186.java
+++ CentricToAll11/CustomizedExceptions/Lab186.java
@@ -1,27 +1,26 @@
 package CentricToAll11.CustomizedExceptions;
 
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 public class Lab186 {
 
-    public static void main(String[] args) throws Exception{      //Note:Throws keyword is used to safeguard the entire code
-         readfile("C://Abhilash.txt");   ///Declaring the params of the function in the main method and then try executing.
+    public static void main(String[] args) throws Exception {    //Note:Throws keyword is used to safeguard the entire code
+        readfile("C://Abhilash.txt"); ///Declaring the params of the function in the main method and then try executing.
         //Vulnerable = Prone to error
+
     }
-
-
-//Defining a function outside the main method
-
+    //Defining a function outside the main method
     static void readfile(String Filename) throws Exception {
         File f = new File(Filename);
         FileReader fileReader = new FileReader(Filename);
-        if (Filename.length() == 0) {
+        if (Filename.length() == 16) {
             throw new FileNotFoundException();
-
         }
         System.out.println("File not found exception");
-
     }
 }
+
+
