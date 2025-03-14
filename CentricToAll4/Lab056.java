513b77518a59a9921785282d90a36f203f450492
AbhilashGUI <vemulaabhilash8433@gmail.com>
2025-03-14 21:39:04 +0530

    Reupdated code

--- CentricToAll4/Lab056.java
+++ CentricToAll4/Lab056.java
@@ -9,6 +9,8 @@ public class Lab056
         System.out.println((60/12)*4+12-7);
         System.out.println((6*12)/4+12-7);
         System.out.println((49/7)*4+(3-2));
+        System.out.println(12*4/3+(15-11));
+
 
 
 
