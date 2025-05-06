f7b412c802fb5ec92568ec7a053ec3dfe9b5476e
AbhilashGUI <vemulaabhilash8433@gmail.com>
2025-05-06 21:21:37 +0530

    Reupdated code

--- AssignmentPrograms/PalindromeCheck.java
+++ AssignmentPrograms/PalindromeCheck.java
@@ -3,26 +3,29 @@ import java.util.Scanner;
 public class PalindromeCheck {
     public static void main(String[] args)
     {
-           //Develop a program to check if a given string or number is a palindrome (reads the same backward as forward).
+           //Develop a program to check if a given number is a palindrome (reads the same backward as forward).
 
-        int temp, remainder, sum = 0;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number");
-        int number = sc.nextInt();
-        temp = number;
+          int temp, remainder,sum=0;
+          System.out.println("Enter the number");
+          Scanner sc= new Scanner(System.in);
+          int number=sc.nextInt();
+          temp=number;
 
-        while (number>0) {
+          while (number>0)
+          {
+              remainder=number%10;
+              sum=sum*10+remainder;
+              number=number/10;
 
-            remainder = number % 10;
-            sum = (sum * 10) + remainder;
-            number = number / 10;
-        }
-        if (temp == sum) {
-            System.out.println("Given number is Palindrome");
-
-        } else {
-            System.out.println("Given number is not Palindrome");
-        }
+          }
+          if(temp==sum)
+          {
+              System.out.println("Given number is a palindrome");
+          }
+          else
+          {
+              System.out.println("Given number is not a palindrome");
+          }
     }
 }
 
