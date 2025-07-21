8aff0ebf8603a12f4e12abec74c9cf528e2a45b3
AbhilashGUI <vemulaabhilash8433@gmail.com>
2025-07-21 23:31:15 +0530

    Reupdated code

--- CentricToAll13/Multithreading/Threads/Lab243.java
+++ CentricToAll13/Multithreading/Threads/Lab243.java
@@ -7,11 +7,11 @@ public class Lab243 {
 
      Thread t=Thread.currentThread();
      System.out.println(t);
-     for(int i=0;i<10;i++)
+     for(int i=0;i<15;i++)
      {
          System.out.println(i+ " "+t.getName());
          System.out.println(i+" "+t.getPriority());
-         Thread.sleep(10000);
+         Thread.sleep(3000);
      }
 
 //Results contains GroupName(1), Mainthread(main) and priority(5 by default)
