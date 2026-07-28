4bcb269ba494826e2826b9532ed8aef0c4846295
AbhilashGUI <vemulaabhilash8433@gmail.com>
2026-07-28 12:31:01 +0530

    Reupdated code

--- CentricToAll13/Multithreading/StackExample/StackWaitNotify.java
+++ CentricToAll13/Multithreading/StackExample/StackWaitNotify.java
@@ -1,92 +1,111 @@
+
 package CentricToAll13.Multithreading.StackExample;
 
-public class StackWaitNotify
-{
-    public static void main(String[] args)
-    {
+
+public class StackWaitNotify {
+    public static void main(String[] args) {
         //Note: We have a stack class with 2 methods.
         //M1=push. In the push method the flag is set to be true,which wait for sometime and do a push method then notify the other thread
         //M2=pop.  In the pop method the flag is set to be false,Still the top element is present, it wait for sometime then notify and return x
+        //Note; Incase of removing synchronized keyword, it  will throw an IllegalMonitorStateException.
 
-        //Note; Incase of removing syn keyword, it  will throw an IllegalMonitorStateException.
-        Stack st = new Stack();
-        B obj2 = new B(st, "B");
-        A obj1 = new A(st, "A");
+        //The producer thread (A) waits whenever the buffer is full, the consumer thread (B) waits whenever the buffer is empty,
+        // and wait()/notify() along with synchronized coordinate the two threads so they alternate safely without corrupting the shared data.
+
+
+        Stack stack= new Stack();
+        M object1= new M(stack,"M");
+        N Object2= new N(stack,"N");
 
     }
-    }
+}
 
-    class Stack {
-        int x;
-        boolean flag = false;
 
-        synchronized void push(int x) {
-            if (flag) {
-                try {
-                    wait();
-                } catch (Exception e) {
-                    System.out.println(e);  
-                }
+
+
+class Stack{
+    int i;
+    boolean flag=false;
+
+    synchronized void push(int i)
+    {
+        if(flag) {
+            try {
+                wait();
+            } catch (Exception e) {
+                System.out.println(e);
+
             }
-            this.x = x;
-            System.out.println(x + " is pushed..");
-
-            flag = true;
+        }
+            this.i=i;
+            System.out.println(i+ " is pushed ");
+            flag=true;
             notify();
         }
 
-        synchronized public int pop() {
-            if (!flag) {
+        synchronized public int pop()
+        {
+            if(!flag)
+            {
                 try {
                     wait();
                 } catch (Exception e) {
                     System.out.println(e);
                 }
             }
-            System.out.println(x + " is poped");
+            System.out.println(i+ " is poped ");
+
             try {
                 Thread.sleep(1000);
             } catch (Exception e) {
                 System.out.println(e);
             }
-            flag = false;
+            flag=false;
             notify();
-            return x;
+            return i;
         }
+
+}
+
+class M implements Runnable {
+    Stack st = null;
+    String name;
+
+    public M(Stack st, String name) {
+        this.st = st;
+        this.name = name;
+
+
+        Thread t1 = new Thread(this, name);
+        t1.start();
 
     }
 
-    class A implements Runnable {
-        Stack st = null;
-
-        A(Stack st, String name) {
-            this.st = st;
-            Thread t1 = new Thread(this, name);
-            t1.start();
-        }
-
-        public void run() {
-            int a = 1;
-            for (int i = 0; i < 10; i++) {
-                st.push(a++);
-            }
+    public void run() {
+        int a = 1;
+        for (int i = 0; i < 5; i++) {
+            st.push(a++);
         }
     }
-
-    class B implements Runnable {
-        Stack st = null;
-
-        B(Stack st, String name) {
+}
+    class N implements Runnable
+    {
+        Stack st= null;
+        String name;
+        public N(Stack st, String name) {
             this.st = st;
-            Thread t2 = new Thread(this, name);
+            this.name = name;
+
+            Thread t2= new Thread(this,name);
             t2.start();
         }
 
+        @Override
         public void run() {
-            for (int i = 0; i < 10; i++) {
+            for (int i=0;i<5;i++)
+            {
                 st.pop();
             }
         }
     }
-
 
